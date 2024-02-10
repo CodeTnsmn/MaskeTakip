569cfadeb21aa68afea561621e8bbca3e4de6574
CodeTnsmn <107807478+CodeTnsmn@users.noreply.github.com>
2024-02-10 14:43:00 +0300

    Add files via upload

---
+++ MaskeTakip/MaskeTakip/Program.cs
@@ -0,0 +1,21 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System.Runtime.Versioning;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person1 = new Person();
+        person1.FirstName = "ali";
+        person1.LastName = "veli";
+        person1.DateOfBirthYear = 1923;
+        person1.NationalIdentity = 123;
+
+        PttManager pttManager = new PttManager(new PersonManager());
+        pttManager.GiveMask(person1);
+        Console.ReadLine();
+    }
+
+
+}
